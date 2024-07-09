3247f8e93a079e5b6730b9caee4bbab0e498594e
albertonoys <albertonoys@gmail.com>
2024-07-09 18:01:35 +0200

    Add tools to toml

---
+++ scripts/execute_template.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+execute_template() {
+    local template_name="$1.tmpl"
+    local chezmoi_source_dir=$(chezmoi source-path)
+    local template_path="$chezmoi_source_dir/.chezmoitemplates/$template_name"
+
+    if [ ! -f "$template_path" ]; then
+        echo "Error: Template file $template_name not found in .chezmoitemplates directory."
+        return 1
+    fi
+
+    # Execute the template content as an inline template
+    chezmoi execute-template < "$template_path" | bash
+}
