3d4fcc5fdea2c115803f5250e4ace33ac21f94ea
albertonoys <albertonoys@gmail.com>
2024-07-09 15:05:46 +0200

    Update fzf

--- run_once_install_fzf.sh
+++ run_once_install_fzf.sh
@@ -3,16 +3,41 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Check if fzf is already installed
-if [ -d "$HOME/.fzf" ]; then
-    echo "fzf is already installed. Skipping installation."
-    exit 0
-fi
+FZF_DIR="$HOME/.fzf"
+FZF_REPO="https://github.com/junegunn/fzf.git"
 
-# Clone fzf repository
-git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
+get_latest_version() {
+    git ls-remote --tags --refs "$FZF_REPO" |
+    cut -d/ -f3 |
+    sort -V |
+    tail -n1
+}
 
-# Run fzf install script
-"$HOME/.fzf/install" --all
+get_installed_version() {
+    if [ -d "$FZF_DIR" ]; then
+        (cd "$FZF_DIR" && git describe --tags --abbrev=0)
+    else
+        echo "Not installed"
+    fi
+}
 
-echo "fzf has been successfully installed."
+install_or_update_fzf() {
+    local latest_version=$(get_latest_version)
+    local installed_version=$(get_installed_version)
+
+    if [ "$installed_version" = "Not installed" ]; then
+        echo "fzf is not installed. Installing now..."
+        git clone --depth 1 "$FZF_REPO" "$FZF_DIR"
+        "$FZF_DIR/install" --all
+        echo "fzf has been successfully installed."
+    elif [ "$installed_version" != "$latest_version" ]; then
+        echo "Updating fzf from $installed_version to $latest_version"
+        (cd "$FZF_DIR" && git fetch && git checkout "$latest_version")
+        "$FZF_DIR/install" --all
+        echo "fzf has been successfully updated."
+    else
+        echo "fzf is already up to date (version $installed_version)."
+    fi
+}
+
+install_or_update_fzf
