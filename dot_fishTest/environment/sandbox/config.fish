55eb9aefd127de1323aa39774ba8f0df5cc84ca2
albertonoys <albertonoys@gmail.com>
2024-07-18 15:03:51 +0200

    Comment out bin folder for now

--- dot_fishTest/environment/sandbox/config.fish
+++ dot_fishTest/environment/sandbox/config.fish
@@ -4,9 +4,9 @@ if not set -q __dotfiles_CODE_DIR
     set -g __dotfiles_CODE_DIR (realpath ~/www/iats/code)
 end
 
-if test -e $__dotfiles_PATH/bin
-    fish_add_path ($__dotfiles_PATH/bin)
-end
+# if test -e $__dotfiles_PATH/bin
+#     fish_add_path ($__dotfiles_PATH/bin)
+# end
 
 # Set initial directory
 cd $__dotfiles_CODE_DIR
