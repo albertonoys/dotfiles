bf790b427368ff7fdacbf8501c8db23d8ea18602
albertonoys <albertonoys@gmail.com>
2024-07-18 17:04:03 +0200

    Check if exists

--- private_dot_config/private_fish/conf.d/atuin.env.fish
+++ private_dot_config/private_fish/conf.d/atuin.env.fish
@@ -1,2 +1,3 @@
-
-source "$HOME/.atuin/bin/env.fish"
+if dir_exists "$HOME/.atuin/bin/env.fish"
+    source "$HOME/.atuin/bin/env.fish"
+end
--- private_dot_config/private_fish/conf.d/rustup.fish
+++ private_dot_config/private_fish/conf.d/rustup.fish
@@ -1,1 +1,3 @@
-source "$HOME/.cargo/env.fish"
+if dir_exists "$HOME/.cargo/env.fish"
+    source "$HOME/.cargo/env.fish"
+end
